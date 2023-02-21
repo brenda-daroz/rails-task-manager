b26c492c0cf1e01df34f72f69466f9328d1fdae3
Brenda Daroz <brenda.daroz@gmail.com>
2023-02-21 15:35:25 +0100

    checkbox not finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Read all
+  get "tasks", to: "tasks#index", as: :tasks
+  # Create new task
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  # Read one task
+  get "tasks/:id", to: "tasks#show", as: :task
+  #Edit one task
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update"
+  #Delete one task
+  delete "tasks/:id", to: "tasks#destroy"
 end
